c5d39f2f42600b4a182a93863132e7ae88d3d6b3
ashishs10 <ashish9six@gmail.com>
2026-04-02 00:29:26 +0530

    added add task

--- index.js
+++ index.js
@@ -1,16 +1,88 @@
-// console.log(process.argv);
-process.stdin.setEncoding("utf-8");
+// #!/usr/bin/env node
 
-process.stdout.write("> ");
+// process.stdin.setEncoding("utf-8");
 
-process.stdin.on("data", (input) => {
-  const trimmed = input.trim();
+// process.stdout.write("> ");
 
-  if (trimmed === "exit") {
-    process.exit(0);
+// process.stdin.on("data", (input) => {
+//   const trimmed = input.trim();
+
+//   if (trimmed === "exit") {
+//     process.exit(0);
+//   }
+
+//   console.log(typeof trimmed);
+//   console.log("You typed : ", trimmed);
+
+//   process.stdout.write("> ");
+// });
+const fs = require("fs/promises");
+
+const userAction = process.argv[2];
+const input = process.argv[3];
+
+async function readFile(file) {
+  try {
+    const data = await fs.readFile(file, "utf-8");
+    const parsedData = JSON.parse(data, null, 2);
+    return parsedData;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function initFile(file) {
+  try {
+    await fs.access(file);
+    const data = await readFile(file);
+
+    console.log("file exists");
+    return data;
+  } catch (err) {
+    try {
+      await fs.writeFile(file, JSON.stringify([], null, 2));
+      console.log("file created");
+      return [];
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
+
+async function writeFile(file, data) {
+  try {
+    await fs.writeFile(file, JSON.stringify(data, null, 2));
+    console.log("task added successfully");
+    return;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// initFile("todo.json");
+
+async function addTask(description) {
+  let data = await initFile("todo.json");
+  if (!description) {
+    console.log("no description of the task given");
+    return;
   }
 
-  console.log("You typed : ", trimmed);
+  if (data.length === 0) {
+    data.push({ description: description, status: "todo", id: 1 });
+    await writeFile("todo.json", data);
+    return;
+  }
 
-  process.stdout.write("> ");
-});
+  // [{},{},{},{}]
+  const id = data.map((task) => task.id).reduce((a, b) => Math.max(a, b)) + 1;
+
+  data.push({ description: description, status: "todo", id });
+  await writeFile("todo.json", data);
+  return;
+}
+
+if (userAction === "add") {
+  addTask(input);
+}
