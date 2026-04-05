31f815a2fd61ed3ea1fbfbd033c01a41d460d279
ashishs10 <ashish9six@gmail.com>
2026-04-05 19:48:52 +0530

    delete task and update task added

--- index.js
+++ index.js
@@ -18,9 +18,22 @@
 // });
 const fs = require("fs/promises");
 
-const userAction = process.argv[2];
 const input = process.argv[3];
 
+const cliInput = process.argv.slice(2);
+
+const userAction = cliInput[0];
+
+let id;
+let description;
+
+if (cliInput.length === 3) {
+  id = cliInput[1];
+  description = cliInput[2];
+} else if (cliInput.length === 2) {
+  description = cliInput[1];
+}
+// -----------------HELPER FUNCTIONS
 async function readFile(file) {
   try {
     const data = await fs.readFile(file, "utf-8");
@@ -59,6 +72,7 @@ async function writeFile(file, data) {
     throw error;
   }
 }
+// -----------------HELPER FUNCTIONS END
 
 // initFile("todo.json");
 // ADD TASK
@@ -76,17 +90,89 @@ async function addTask(description) {
   }
 
   // [{},{},{},{}]
-  const id = data.map((task) => task.id).reduce((a, b) => Math.max(a, b)) + 1;
+  const id =
+    data.length > 0
+      ? data.map((task) => task.id).reduce((a, b) => Math.max(a, b)) + 1
+      : 0;
 
   data.push({ description: description, status: "todo", id });
   await writeFile("todo.json", data);
   return;
 }
 
-async function updateTask(description) {}
+async function updateTask(description, id) {
+  // const task =
+  try {
+    const data = await initFile("todo.json");
+    console.log("data : ", data);
+
+    const taskIndex = data.findIndex((task) => task.id === Number(id));
+    console.log("task index : ", taskIndex);
+
+    if (taskIndex === -1) {
+      console.log("No task exist with id");
+      return;
+    }
+
+    data[taskIndex].description = description;
+    await writeFile("todo.json", data);
+    console.log("Task updated");
+    return;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function deleteTask(id) {
+  try {
+    const taskId = Number(id);
+
+    const tasks = await initFile("todo.json");
+
+    // getting the index of the task
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+    if (taskIndex === -1) {
+      throw Error("Wrong task ID");
+    }
+    const remainingTasks = tasks.filter((task) => task.id !== taskId);
+    console.log("Remaining tasks : ", remainingTasks);
+
+    await writeFile("todo.json", remainingTasks);
+    return;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function showAllTask() {
+  try {
+    const data = await readFile("todo.json");
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 if (userAction === "add") {
-  addTask(input);
+  addTask(description);
 } else if (userAction === "update") {
-  updateTask(input);
+  console.log(cliInput.length);
+  if (cliInput.length != 3) {
+    console.log("Some arguments missing");
+    return;
+  }
+  if (!id) {
+    console.log("No id given");
+  }
+
+  const updateId = Number.isNaN(Number(input));
+  if (!updateId) {
+    updateTask(description, id);
+  }
+  console.log(updateId);
+} else if (userAction === "delete") {
+  deleteTask(description);
+} else if (userAction === "list") {
+  showAllTask();
 }
